3b21682016f49112e24613c88f7a2faafc038e84
zzangjyj0818 <jyj010818@naver.com>
2025-09-01 03:21:10 +0900

    [#8] Refactor: 이미지 관리 클래스 구현으로 인한 정보나눔 피드 생성 코드 수정

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/information/service/InformationGenerator.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/information/service/InformationGenerator.java
@@ -1,31 +1,25 @@
 package org.goormthon.seasonthon.nocheongmaru.domain.information.service;
 
 import lombok.RequiredArgsConstructor;
-import org.goormthon.seasonthon.nocheongmaru.domain.image.entity.Image;
-import org.goormthon.seasonthon.nocheongmaru.domain.image.repository.ImageRepository;
+import org.goormthon.seasonthon.nocheongmaru.domain.image.service.ImageManager;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.entity.Category;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.entity.Information;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.provider.KakaoGeocodingProvider;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.provider.dto.GeocodingResponse;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.repository.InformationRepository;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.entity.Member;
-import org.goormthon.seasonthon.nocheongmaru.global.s3.S3StorageUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
 public class InformationGenerator {
     
     private final InformationRepository informationRepository;
-    private final ImageRepository imageRepository;
-    private final S3StorageUtil s3StorageUtil;
+    private final ImageManager imageManager;
     private final KakaoGeocodingProvider kakaoGeocodingProvider;
     
     @Transactional
@@ -35,16 +29,7 @@ public class InformationGenerator {
         Information information = createInformation(member, title, description, address, category, geo);
         informationRepository.save(information);
         
-        List<Image> imageList = Optional.ofNullable(images)
-            .orElse(Collections.emptyList())
-            .stream()
-            .filter(file -> file != null && !file.isEmpty())
-            .map(file -> createImage(information, file))
-            .toList();
-        
-        if (!imageList.isEmpty()) {
-            imageRepository.saveAll(imageList);
-        }
+        imageManager.saveImages(information, images);
         
         return information.getId();
     }
@@ -58,14 +43,6 @@ public class InformationGenerator {
             .latitude(geo.latitude())
             .longitude(geo.longitude())
             .category(category)
-            .build();
-    }
-    
-    private Image createImage(Information information, MultipartFile imageFile) {
-        String fileName = s3StorageUtil.uploadFileToS3(imageFile);
-        return Image.builder()
-            .imageUrl(fileName)
-            .information(information)
             .build();
     }
     
--- src/test/java/org/goormthon/seasonthon/nocheongmaru/domain/information/service/InformationGeneratorTest.java
+++ src/test/java/org/goormthon/seasonthon/nocheongmaru/domain/information/service/InformationGeneratorTest.java
@@ -2,7 +2,7 @@ package org.goormthon.seasonthon.nocheongmaru.domain.information.service;
 
 import org.goormthon.seasonthon.nocheongmaru.IntegrationTestSupport;
 import org.goormthon.seasonthon.nocheongmaru.domain.image.entity.Image;
-import org.goormthon.seasonthon.nocheongmaru.domain.image.repository.ImageRepository;
+import org.goormthon.seasonthon.nocheongmaru.domain.image.service.ImageManager;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.entity.Category;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.entity.Information;
 import org.goormthon.seasonthon.nocheongmaru.domain.information.provider.KakaoGeocodingProvider;
@@ -11,7 +11,6 @@ import org.goormthon.seasonthon.nocheongmaru.domain.information.repository.Infor
 import org.goormthon.seasonthon.nocheongmaru.domain.member.entity.Member;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.entity.Role;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.repository.MemberRepository;
-import org.goormthon.seasonthon.nocheongmaru.global.s3.S3StorageUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,8 +22,11 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 class InformationGeneratorTest extends IntegrationTestSupport {
     
@@ -35,20 +37,16 @@ class InformationGeneratorTest extends IntegrationTestSupport {
     private InformationRepository informationRepository;
     
     @Autowired
-    private ImageRepository imageRepository;
-    
-    @Autowired
     private InformationGenerator informationGenerator;
     
     @MockitoBean
     private KakaoGeocodingProvider kakaoGeocodingProvider;
     
     @MockitoBean
-    private S3StorageUtil s3StorageUtil;
+    private ImageManager imageManager;
     
     @AfterEach
     void tearDown() {
-        imageRepository.deleteAllInBatch();
         informationRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
     }
@@ -69,8 +67,7 @@ class InformationGeneratorTest extends IntegrationTestSupport {
         
         MockMultipartFile image1 = createMockFile("image1", "image1");
         MockMultipartFile image2 = createMockFile("image2", "image2");
-        given(s3StorageUtil.uploadFileToS3(image1)).willReturn("s3://bucket-name/image1.png");
-        given(s3StorageUtil.uploadFileToS3(image2)).willReturn("s3://bucket-name/image2.png");
+        
         
         // when
         Long informationId = informationGenerator.generate(
@@ -84,12 +81,10 @@ class InformationGeneratorTest extends IntegrationTestSupport {
         
         // then
         Information information = informationRepository.findById(informationId);
-        List<Image> images = imageRepository.findAllByInformationId(informationId);
-        
         assertThat(information)
             .extracting("id", "title", "description", "address", "latitude", "longitude", "category")
             .containsExactly(information.getId(), "title", "description", "address", 37.123456, 127.123456, Category.HOSPITAL_FACILITIES);
-        assertThat(images).hasSize(2);
+        verify(imageManager).saveImages(any(Information.class), eq(List.of(image1, image2)));
     }
     
     @DisplayName("정보나눔 피드 생성 시 이미지는 없어도 된다.")
@@ -106,7 +101,6 @@ class InformationGeneratorTest extends IntegrationTestSupport {
         given(kakaoGeocodingProvider.getGeocodingByAddress("address"))
             .willReturn(geocodingResponse);
         
-        
         // when
         Long informationId = informationGenerator.generate(
             member,
@@ -119,12 +113,10 @@ class InformationGeneratorTest extends IntegrationTestSupport {
         
         // then
         Information information = informationRepository.findById(informationId);
-        List<Image> images = imageRepository.findAllByInformationId(informationId);
-        
         assertThat(information)
             .extracting("id", "title", "description", "address", "latitude", "longitude", "category")
             .containsExactly(information.getId(), "title", "description", "address", 37.123456, 127.123456, Category.HOSPITAL_FACILITIES);
-        assertThat(images).hasSize(0);
+        verify(imageManager).saveImages(any(Information.class), isNull());
     }
     
     private Member createMember() {
