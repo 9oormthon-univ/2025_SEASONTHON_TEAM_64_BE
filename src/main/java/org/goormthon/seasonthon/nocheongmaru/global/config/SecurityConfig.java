8eb1c4db17ea2d11dedc9e1a771550d5ef025e1d
zzangjyj0818 <jyj010818@naver.com>
2025-09-03 05:19:33 +0900

    [#13] Fix: 배포 환경에서 H2 DB 차단

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/global/config/SecurityConfig.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.goormthon.seasonthon.nocheongmaru.global.security.jwt.provider.TokenP
 import org.goormthon.seasonthon.nocheongmaru.global.security.oauth.OAuth2KakaoService;
 import org.goormthon.seasonthon.nocheongmaru.global.security.oauth.handler.Oauth2FailureHandler;
 import org.goormthon.seasonthon.nocheongmaru.global.security.oauth.handler.Oauth2SuccessHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -93,9 +94,9 @@ public class SecurityConfig {
     }
     
     @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
     public WebSecurityCustomizer configureH2ConsoleEnable() {
-        return web -> web.ignoring()
-            .requestMatchers(PathRequest.toH2Console());
+        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
     }
     
 }
