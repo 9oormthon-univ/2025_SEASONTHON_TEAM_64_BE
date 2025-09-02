fb1ee9a5fd670d9a3e4355ef853acfb67b018b7f
jae-2024 <dbswotkd2019@naver.com>
2025-09-02 19:42:21 +0900

    [#10] fix: Response에 memberEmail 삭제

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/member/model/response/MemberResponse.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/member/model/response/MemberResponse.java
@@ -4,14 +4,12 @@ import org.goormthon.seasonthon.nocheongmaru.domain.member.model.entity.Member;
 
 public record MemberResponse(
 	Long id,
-	String email,
 	String nickname,
 	String profileImageUrl
 ) {
 	public static MemberResponse of(Member member) {
 		return new MemberResponse(
 			member.getId(),
-			member.getEmail(),
 			member.getNickname(),
 			member.getProfileImageURL()
 		);
