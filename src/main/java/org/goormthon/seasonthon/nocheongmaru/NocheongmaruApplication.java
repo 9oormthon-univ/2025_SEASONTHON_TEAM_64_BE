35ed9d9d98d883238a5ca2391f0a408e9a065638
jae-2024 <dbswotkd2019@naver.com>
2025-09-04 00:55:16 +0900

    [#12] feat: 알림설정 추가

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/NocheongmaruApplication.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/NocheongmaruApplication.java
@@ -2,8 +2,10 @@ package org.goormthon.seasonthon.nocheongmaru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NocheongmaruApplication {
     
     public static void main(String[] args) {
