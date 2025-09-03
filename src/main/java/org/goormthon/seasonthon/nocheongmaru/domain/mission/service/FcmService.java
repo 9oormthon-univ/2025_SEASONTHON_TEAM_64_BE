f83f444b5f77690a32a9843ef7d08ca9212329cd
jae-2024 <dbswotkd2019@naver.com>
2025-09-04 00:49:55 +0900

    [#12] feat: 미션 구현

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/mission/service/FcmService.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/mission/service/FcmService.java
@@ -11,7 +11,7 @@ public class FcmService {
 
 	public void sendTodayMission(Member member, Mission mission, LocalDate forDate) {
 		if (member.getFcmToken() == null)
-			return; // 토큰 없는 유저는 스킵
+			return;
 
 		String title = "오늘의 미션이 도착했어요!";
 		String body = "[" + forDate + "] " + mission.getTitle();
