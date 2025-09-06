c73dbe772208e9cff105144bc4bb996dc44231ea
zzangjyj0818 <jyj010818@naver.com>
2025-09-06 16:25:00 +0900

    [#24] Feat: 회원 정보 반환 기능 구현

--- src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/member/controller/MemberController.java
+++ src/main/java/org/goormthon/seasonthon/nocheongmaru/domain/member/controller/MemberController.java
@@ -2,14 +2,13 @@ package org.goormthon.seasonthon.nocheongmaru.domain.member.controller;
 
 import java.util.Map;
 
+import lombok.Getter;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.controller.docs.MemberControllerDocs;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.service.MemberService;
+import org.goormthon.seasonthon.nocheongmaru.domain.member.service.dto.response.MemberDetailResponse;
+import org.goormthon.seasonthon.nocheongmaru.global.annotation.AuthMemberId;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,20 +16,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
 public class MemberController implements MemberControllerDocs {
-
-	private final MemberService memberService;
-
-	@PostMapping("/{memberId}/fcm-token")
-	public ResponseEntity<Map<String, String>> registerFcmToken(
-		@PathVariable Long memberId,
-		@RequestBody Map<String, String> request
-	) {
-		String token = request.get("token");
-		if (token == null || token.isBlank()) {
-			return ResponseEntity.badRequest().body(Map.of("message", "토큰이 비어 있습니다."));
-		}
-
-		memberService.updateFcmToken(memberId, token);
-		return ResponseEntity.ok(Map.of("message", "FCM 토큰이 정상적으로 등록되었습니다."));
-	}
+    
+    private final MemberService memberService;
+    
+    @GetMapping
+    public ResponseEntity<MemberDetailResponse> getMemberDetail(
+        @AuthMemberId Long memberId
+    ) {
+        return ResponseEntity.ok(memberService.getMemberDetail(memberId));
+    }
+    
+    @PostMapping("/{memberId}/fcm-token")
+    public ResponseEntity<Map<String, String>> registerFcmToken(
+        @PathVariable Long memberId,
+        @RequestBody Map<String, String> request
+    ) {
+        String token = request.get("token");
+        if (token == null || token.isBlank()) {
+            return ResponseEntity.badRequest().body(Map.of("message", "토큰이 비어 있습니다."));
+        }
+        
+        memberService.updateFcmToken(memberId, token);
+        return ResponseEntity.ok(Map.of("message", "FCM 토큰이 정상적으로 등록되었습니다."));
+    }
 }
