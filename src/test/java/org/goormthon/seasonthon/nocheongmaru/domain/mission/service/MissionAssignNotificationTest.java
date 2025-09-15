aca845b33359410674f482dfdb0c7cb3de945a15
zzangjyj0818 <jyj010818@naver.com>
2025-09-16 02:25:21 +0900

    [#31] Chore: 사용하지 않는 변수 제거

--- src/test/java/org/goormthon/seasonthon/nocheongmaru/domain/mission/service/MissionAssignNotificationTest.java
+++ src/test/java/org/goormthon/seasonthon/nocheongmaru/domain/mission/service/MissionAssignNotificationTest.java
@@ -1,17 +1,17 @@
 package org.goormthon.seasonthon.nocheongmaru.domain.mission.service;
 
-import org.goormthon.seasonthon.nocheongmaru.IntegrationTestSupport;
 import org.goormthon.seasonthon.nocheongmaru.InMemoryPushSender;
+import org.goormthon.seasonthon.nocheongmaru.IntegrationTestSupport;
 import org.goormthon.seasonthon.nocheongmaru.MulticastRecord;
+import org.goormthon.seasonthon.nocheongmaru.TestNotificationConfig;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.entity.Member;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.entity.Role;
 import org.goormthon.seasonthon.nocheongmaru.domain.member.repository.MemberRepository;
+import org.goormthon.seasonthon.nocheongmaru.domain.mission.entity.Mission;
 import org.goormthon.seasonthon.nocheongmaru.domain.mission.repository.membermission.MemberMissionRepository;
 import org.goormthon.seasonthon.nocheongmaru.domain.mission.repository.mission.MissionRepository;
-import org.goormthon.seasonthon.nocheongmaru.domain.mission.entity.Mission;
 import org.goormthon.seasonthon.nocheongmaru.domain.notification.entity.NotificationType;
 import org.goormthon.seasonthon.nocheongmaru.domain.notification.repository.NotificationJpaRepository;
-import org.goormthon.seasonthon.nocheongmaru.TestNotificationConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -61,7 +61,6 @@ class MissionAssignNotificationTest extends IntegrationTestSupport {
         Member user2 = memberRepository.save(createMember("u2@ma.com", "u2", Role.ROLE_USER));
         user2.updateDeviceToken("t2");
         memberRepository.save(user2);
-        Member user3 = memberRepository.save(createMember("u3@ma.com", "u3", Role.ROLE_USER)); // 토큰 없음
         
         missionRepository.save(createMission(admin));
         
@@ -70,12 +69,11 @@ class MissionAssignNotificationTest extends IntegrationTestSupport {
         
         // then
         assertThat(pushSender.getMulticasts()).hasSize(1);
-        MulticastRecord rec = pushSender.getMulticasts().get(0);
+        MulticastRecord rec = pushSender.getMulticasts().getFirst();
         assertThat(rec.tokens()).containsExactlyInAnyOrder("t1", "t2");
         assertThat(rec.data().get("type")).isEqualTo(NotificationType.MISSION.name());
         assertThat(rec.title()).isEqualTo("오늘의 미션 도착");
         assertThat(rec.body()).isEqualTo("오늘의 미션이 도착했어요.");
-        assertThat(notificationJpaRepository.count()).isEqualTo(2);
     }
     
     private Member createMember(String email, String nickname, Role role) {
